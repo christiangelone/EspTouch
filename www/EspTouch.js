acc352628688008511655d47b4235d6634cbff63
Christian Angelone <christiangelone@gmail.com>
2021-09-16 00:24:28 -0300

    Adds console logs to verify bridge call

--- www/EspTouch.js
+++ www/EspTouch.js
@@ -2,9 +2,11 @@ var exec = require('cordova/exec');
 
 module.exports = {
     start: function (apSsid, apBssid, apPassword, deviceCountData, broadcastData, successCallback, failCallback) {
+      console.log("EspTouch.start")
       exec(successCallback, failCallback, "EspTouch", "start", [apSsid, apBssid, apPassword, deviceCountData, broadcastData]);
     },
     stop: function (successCallback, failCallback) {
+      console.log("EspTouch.stop")
       exec(successCallback, failCallback, "EspTouch", "stop", []);
     }
 }
